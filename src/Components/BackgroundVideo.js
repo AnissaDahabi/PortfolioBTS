96ab909543d9312ac0632b9a382abbda3774548e
Anissa <anissa.dahabi@gmail.com>
2025-07-11 17:54:42 +0200

    Revert "Merge branch 'gh-pages' of https://github.com/AnissaDahabi/PortfolioBTS into gh-pages"
    This reverts commit 034cea4158d80c7c1835d2e851962a603a7b9e9f, reversing
    changes made to c1f5d6271f6e832e1a9fa9534de27739ac4ebd76.

---
+++ src/Components/BackgroundVideo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import videoFile from '../Assets/video.mp4'
+
+export default function BackgroundVideo() {
+    return (
+        <div>
+            <video autoPlay loop muted className="video">
+                <source src={videoFile} type="video/mp4"/>
+            </video>
+        </div>
+    );
+}
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './Assets/StyleSheets/style.css';
+import MainPage from './Components/MainPage.js';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <MainPage />
+  </React.StrictMode>
+);
+
+reportWebVitals();
